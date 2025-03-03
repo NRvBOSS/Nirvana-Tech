f2e0d5ce5fe749c587b4e833f808621444339aec
NRvCODERPRO <140975114+NRvCODERPRO@users.noreply.github.com>
2025-03-03 12:04:46 +0400

    add somethings on main page

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
 import AppHeader from './views/AppHeader.vue';
+import AppMain from './views/AppMain.vue';
 
 </script>
 
 <template>
 <AppHeader />
+<AppMain />
 </template>
---
+++ src/views/AppMain.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    class="relative w-full h-[300px] bg-[url('../photos\colorful-banner-with-hands-working-on-computer-vector-21894954.jpg')] bg-cover bg-center"
+  >
+    <div class="absolute left-10 top-1/3 bg-white w-1/3 h-1/3 p-3 pl-10 shadow-2xl rounded-3xl">
+      <h2 class="font-bold text-3xl text-gray-800">Nirvana Tech</h2>
+      <div class="w-12 h-1 bg-blue-500 mt-2">
+        <p class="pt-2 text-xl w-40 text-gray-800">Do it yourself!</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="">
+    <h3 class="flex justify-center p-20 text-6xl font-bold text-blue-500">
+        About
+    </h3>
+    <p class="flex justify-center p-8 text-2xl text-center">
+        Nirvana Tech is my first project and a source of motivation. I started building various projects using different languages and frameworks. I plan to share them here and on my GitHub profile.This journey is long, challenging, and full of learning. The main goal of this company is not to earn money but to gain valuable experience. If you want to be part of this journey and grow your skills, join us at Nirvana Tech.
+    </p>
+  </div>
+
+</template>
